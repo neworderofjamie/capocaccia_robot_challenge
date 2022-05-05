0a29ddd884fa097b7639590a9d2a29d1a76730d1
neworderofjamie <neworderofjamie@gmail.com>
2022-05-05 11:45:28 +0200

    first pass at low-resolution horizontal only optical flow

---
+++ parameters.h
@@ -0,0 +1,43 @@
+#pragma once
+
+//------------------------------------------------------------------------
+// Parameters
+//------------------------------------------------------------------------
+namespace Parameters
+{
+	enum DetectorAxis
+	{
+		DetectorAxisHorizontal,
+		DetectorAxisMax,
+	};
+	
+    // Order of detectors associated with each pixel
+    enum Detector
+    {
+        DetectorLeft,
+        DetectorRight,
+        DetectorMax,
+    };
+
+    constexpr double timestep = 1.0;
+
+    constexpr unsigned int inputWidth = 160;
+	constexpr unsigned int inputHeight = 60;
+    constexpr unsigned int kernelSize = 5;
+    constexpr unsigned int centreWidth = 155;
+	constexpr unsigned int centreHeight = 55;
+
+    constexpr unsigned int macroPixelWidth = centreWidth / kernelSize;
+	constexpr unsigned int macroPixelHeight = centreHeight / kernelSize;
+
+    constexpr unsigned int detectorWidth = macroPixelWidth - 2;
+	constexpr unsigned int detectorHeight = macroPixelHeight - 2;
+
+    constexpr unsigned int outputScale = 12;
+    constexpr unsigned int inputScale = 1;
+
+    constexpr float flowPersistence = 0.995f;
+    constexpr float spikePersistence = 0.97f;
+    
+    constexpr float outputVectorScale = 2.0f;
+}
